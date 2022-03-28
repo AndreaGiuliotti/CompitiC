8bb13e49dac4cd5461cc35f69ee246769c07589a
Andrea Giuliotti <andrea.giuliotti@outlook.it>
2022-03-28 05:33:31 -0700

    Primo commit : utilizzo delle operazioni bitwise

---
+++ file6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int n1 = 9;
+int n2 = 3;
+
+printf("%d \n",(n1&n2)); //risultato =  1
+printf("%d \n",(n1|n2)); //risultato = 11
+printf("%d \n",(n1^n2)); //risultato = 10
+
+return 0;
+
+}
